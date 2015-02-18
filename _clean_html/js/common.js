3ab4aa22b729dff099a424d0ffacde96eaa5431b
agragregra <agragregra@ya.ru>
2015-02-19 06:57:13 +0800

    19.02.2015 6:57

--- _clean_html/js/common.js
+++ _clean_html/js/common.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
 
-	
+	function heightDetect() {
+		$(".main_head").css("height", $(window).height());
+	};
+	heightDetect();
+	$(window).resize(function() {
+		heightDetect();
+	});
+
+	$(".toggle_mnu, .menu_item").click(function() {
+		$(".sandwich").toggleClass("active");
+	});
+
+	$(".toggle_mnu").click(function() {
+		if ($(".top_mnu").is(":visible")) {
+			$(".top_mnu").fadeOut(600);
+			$(".top_mnu li").removeClass("fadeInUp animated");
+		} else {
+			$(".top_mnu").fadeIn(600);
+			$(".top_mnu li").addClass("fadeInUp animated");
+		};
+		
+	});
 	
 });
+$(window).load(function() {
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+}); 
