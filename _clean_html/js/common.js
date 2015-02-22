38e0233b9b32ed3d91dde85d8134f2177cdc3dfe
agragregra <agragregra@ya.ru>
2015-02-22 11:57:59 +0800

    Portfolio

--- _clean_html/js/common.js
+++ _clean_html/js/common.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
 
+	$("#portfolio_grid").mixItUp();
+
+	$(".s_portfolio li").click(function() {
+		$(".s_portfolio li").removeClass("active");
+		$(this).addClass("active");
+	});
+
 	$(".popup").magnificPopup({type:"image"});
+	$(".popup_content").magnificPopup({type:"inline", midClick: true});
 
 	$(".section_header").animated("fadeInUp", "fadeOutDown");
 
@@ -39,6 +47,11 @@ $(document).ready(function() {
 			$(".top_mnu").fadeIn(600);
 			$(".top_mnu li a").addClass("fadeInUp animated");
 		};
+	});
+
+	$(".portfolio_item").each(function(i) {
+		$(this).find("a").attr("href", "#work_" + i);
+		$(this).find(".podrt_descr").attr("id", "work_" + i);
 	});
 
 });
